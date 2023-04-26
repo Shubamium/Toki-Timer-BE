634b71101c411f54835309c6d74fcf581b34e565
Vincent Liussyaputra <liusvinv@gmail.com>
2023-04-27 00:05:39 +0700

    Add error checking

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const cors = require('cors');
 // Create a new instance of the Express app
 const app = express();
 app.use(cors());
+app.get('/',(req,res)=>{
+  res.send('Api is running correctly!');
+});
 // Define a route handler for the root URL
 app.get('/home', async (req, res) => {
+  const lat = req.query.lat;
+  const long = req.query.long;
     try{
-      const lat = req.query.lat;
-      const long = req.query.long;
       // const route = `http://api.positionstack.com/v1/reverse?access_key=e1f5483fcad8a99f2f94b4739460b29f&query=-6.2686%2C106.9423&limit=1&output=json&timezone_module=1`;
-  //   res.send('Hello World!');
+       //   res.send('Hello World!');
       const apiKey = 'e1f5483fcad8a99f2f94b4739460b29f';
       const url = new URL('http://api.positionstack.com/v1/reverse')
       url.searchParams.append('access_key',apiKey);
@@ -24,6 +27,7 @@ app.get('/home', async (req, res) => {
       url.searchParams.append('timezone_module','1');
       const result = await axios.get(url);
       res.send(result.data);
+      console.log({lat,long});
     }catch(err){
       res.send(err);
     }

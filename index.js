9c4dc23ca6dd199290b3ea3977e619db193a4a8d
Vincent Liussyaputra <liusvinv@gmail.com>
2023-04-26 23:58:25 +0700

    Add error handler

--- index.js
+++ index.js
@@ -8,21 +8,25 @@ const app = express();
 app.use(cors());
 // Define a route handler for the root URL
 app.get('/home', async (req, res) => {
-    const lat = req.query.lat;
-    const long = req.query.long;
-    // const route = `http://api.positionstack.com/v1/reverse?access_key=e1f5483fcad8a99f2f94b4739460b29f&query=-6.2686%2C106.9423&limit=1&output=json&timezone_module=1`;
-//   res.send('Hello World!');
-    const apiKey = 'e1f5483fcad8a99f2f94b4739460b29f';
-    const url = new URL('http://api.positionstack.com/v1/reverse')
-    url.searchParams.append('access_key',apiKey);
-    const coord = lat + ',' + long;
-    url.searchParams.append('query',coord);
+    try{
+      const lat = req.query.lat;
+      const long = req.query.long;
+      // const route = `http://api.positionstack.com/v1/reverse?access_key=e1f5483fcad8a99f2f94b4739460b29f&query=-6.2686%2C106.9423&limit=1&output=json&timezone_module=1`;
+  //   res.send('Hello World!');
+      const apiKey = 'e1f5483fcad8a99f2f94b4739460b29f';
+      const url = new URL('http://api.positionstack.com/v1/reverse')
+      url.searchParams.append('access_key',apiKey);
+      const coord = lat + ',' + long;
+      url.searchParams.append('query',coord);
 
-    url.searchParams.append('limit',1);
-    url.searchParams.append('output','json');
-    url.searchParams.append('timezone_module','1');
-    const result = await axios.get(url);
-    res.send(result.data);
+      url.searchParams.append('limit',1);
+      url.searchParams.append('output','json');
+      url.searchParams.append('timezone_module','1');
+      const result = await axios.get(url);
+      res.send(result.data);
+    }catch(err){
+      res.send(err);
+    }
 });
 
 // Start the server

bb028448756f787a6eb7d9494bdffeaa081a8189
Vincent Liussyaputra <liusvinv@gmail.com>
2023-04-27 00:38:29 +0700

    Add the search routes

--- index.js
+++ index.js
@@ -33,6 +33,29 @@ app.get('/home', async (req, res) => {
     }
 });
 
+
+app.get('/search', async (req, res) => {
+  const name = req.query.name;
+    try{
+      // const route = `http://api.positionstack.com/v1/reverse?access_key=e1f5483fcad8a99f2f94b4739460b29f&query=-6.2686%2C106.9423&limit=1&output=json&timezone_module=1`;
+       //   res.send('Hello World!');
+      const apiKey = 'e1f5483fcad8a99f2f94b4739460b29f';
+      // Construc the endpoint
+      const url = new URL('http://api.positionstack.com/v1/forward')
+      url.searchParams.append('access_key',apiKey);
+      url.searchParams.append('query',name);
+      url.searchParams.append('limit','5');
+      url.searchParams.append('output','json');
+      url.searchParams.append('timezone_module','1');
+      
+      const result = await axios.get(url);
+      res.send(result.data);
+      console.log({name});
+    }catch(err){
+      res.send(err);
+    }
+});
+
 // Start the server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
